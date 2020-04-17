cb816c2fd10c5bce8330c60622eee69f1b2804c0
delu2000-cloud <63297309+delu2000-cloud@users.noreply.github.com>
2020-04-17 11:18:21 +0100

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+from . import views
+
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
