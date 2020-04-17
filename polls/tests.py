40b9b7a2f64fe9c4cd265c50d1c95ab315a9efeb
delu2000-cloud <63297309+delu2000-cloud@users.noreply.github.com>
2020-04-17 11:22:19 +0100

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -9,6 +9,5 @@ from . import views
 class HomePageTests(SimpleTestCase):
 
     def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEquals(response.status_code, 200)
-
+        response = self.client.get('/admin')
+        self.assertEquals(response.status_code, 301)
